2f4b2597ea5b4b615ac3b745011e89b31d5ce229
jubumiru <olo.olo.olo@windowslive.com>
2018-12-10 10:15:52 +0700

    present2
    present2

--- member_cart.php
+++ member_cart.php
@@ -18,8 +18,8 @@
 
     <div class="w3-center w3-xlarge" style="margin-top:20px; "><i class="fa fa-shopping-cart"></i> ตะกร้าของฉัน</div>
     <div class="w3-row-padding w3-container  w3-center" style="margin-top:20px; margin-left:150px; margin-right:150px ">
-    <!-- ตารางสินค้า     -->
-    <table class="w3-table-all">
+        <!-- ตารางสินค้า     -->
+        <table class="w3-table-all">
             <thead>
                 <tr class="w3-theme-l3">
                     <th>รูปสินค้า</th>
@@ -35,7 +35,9 @@
                 <td><input type="number" min="1" max="50" name="amount"> ชิ้น</p>
                 </td>
                 <td></td>
-                <td><button class="w3-btn w3-small w3-red"><a href="member_payment.php"><i class="fa fa-trash"></i></a></button></td>
+                <td><button class="w3-btn w3-small w3-red" onclick="document.getElementById('premove').style.display='block'"><a
+                            href=""><i class="fa fa-trash"></i></a></button></td>
+
             </tr>
         </table>
     </div>
@@ -45,6 +47,18 @@
         <button class="w3-btn w3-red w3-center w3-round-large" style="margin-top:20px;margin-bottom:10px;"><a href="product.php">สั่งซื้อเพิ่ม</a></button>
     </div>
 
+    <div id="premove" class="w3-modal">
+        <div class="w3-modal-content w3-animate-zoom w3-padding-small w3-round" style="width:40%;">
+            <div class="w3-container w3-white w3-center">
+                <a href=""><i onclick="document.getElementById('premove').style.display='none'" class="fa fa-remove w3-button w3-xlarge w3-right w3-transparent"></i></a>
+                <h2>ยืนยันการลบ ?</h2>
+                <div class="w3-container w3-center w3-section ">
+                    <a href="" class="w3-button w3-round w3-theme"><i class=""></i>ตกลง</a>
+                    <a href="" class="w3-button w3-round w3-light-grey"><i class=""></i> ยกเลิก</a>
+                </div>
+                </div>
+            </div>
+        </div>
 
 </body>
 
